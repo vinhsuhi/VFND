d4dea40881a50a5c63f41a8de79dd9d335ed7b1b
vinhsuhi <vinhbachkhoait@gmail.com>
2020-04-27 08:17:01 +0700

    fix bugs

--- news_clawler.py
+++ news_clawler.py
@@ -15,13 +15,27 @@ def get_data(path, destination):
     links_list = list(links_list)
     final_outputs = {}
     important_keys = ['authors', 'date_publish', 'description', 'image_url', 'language', 'title', 'maintext']
-    for key in tqdm(links_list):
-        value = NewsPlease.from_url(key, timeout=4)
-        paper_data = {}
-        for im_key in important_keys:
-            paper_data[im_key] = value.__dict__[im_key]
-        final_outputs[key] = paper_data
+
+    multiple_index = 20
+    for i in range(links_list//multiple_index):
+        keys = links_list[i * multiple_index : (i+1) * multiple_index]
+        values = NewsPlease.from_urls(keys, timeout=6)
+        for key, value in values:
+            paper_data = {}
+            for im_key in important_keys:
+                paper_data[key] = value.__dict__[im_key]
+            final_outputs[key] = paper_data
     pickle.dump(final_outputs, open(destination, 'wb'))
+    
+
+
+    # for key in tqdm(links_list):
+    #     value = NewsPlease.from_url(key, timeout=4)
+    #     paper_data = {}
+    #     for im_key in important_keys:
+    #         paper_data[im_key] = value.__dict__[im_key]
+    #     final_outputs[key] = paper_data
+    # pickle.dump(final_outputs, open(destination, 'wb'))
 
 if __name__ == "__main__":
     news_links_path = 'Dataset/news_links'
@@ -33,5 +47,6 @@ if __name__ == "__main__":
     files = os.listdir(news_links_path)
 
     for file in files:
-        print("Crawl for file: {}".format(file))
-        get_data(news_links_path + '/' + file, destination=to_save_path + '/' + file[14:-4] + '.pkl')
+        if not os.path.exists(to_save_path + '/' + file[14:-4] + '.pkl'):
+            print("Crawl for file: {}".format(file))
+            get_data(news_links_path + '/' + file, destination=to_save_path + '/' + file[14:-4] + '.pkl')
